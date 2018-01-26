fe816ca034dbb9914b644989bbc4b4ec5881caae
xiang <xiangzz159@qq.com>
2018-01-26 10:33:50 +0800

    update

--- proj1_img2str/ascii.py
+++ proj1_img2str/ascii.py
@@ -8,37 +8,58 @@ from PIL import Image
 
 IMG = 'ascii_dora.png'
 
-ascii_char = ' `01'
-count = len(ascii_char)
+ascii_char1 = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+ascii_char2 = ' `01'
+count1 = len(ascii_char1)
+count2 = len(ascii_char2)
+
+def img2str1():
+    im = Image.open(IMG).resize((153, 153), Image.NEAREST)
+    txt = ""
+    for i in range(im.size[1]):
+        for j in range(im.size[0]):
+            txt += get_char1(*im.getpixel((j, i)))
+        txt += '\n'
+
+    print(txt)
+    f = open("output1.txt", 'w')
+    f.write(txt)
+    f.close()
+
+def get_char1(r,b,g,alpha = 256):
+	if alpha == 0:
+		return ' '
+	gray = int(0.2126*r+0.7152*g+0.0722*b)
+
+	unit = (256.0+1)/count1
+	return ascii_char1[int(gray/unit)]
 
 
-def get_char(r, b, alpha=256):
-    if alpha == 0:
-        return ' '
-    length = len(ascii_char)
-    gray = int(0.7278 * r + 0.1722 * b)
-
-    unit = (256.0 + 1) / length
-    return ascii_char[int(gray / unit)]
-
-def open_img(image_path):
-    im = Image.open(image_path).convert('L')   # 打开图像并转灰度
-    if im.size[0]>1000 or im.size[1]>1000:
-        im = im.resize((int(im.size[0] * 0.1), int(im.size[1] * 0.05)), Image.NEAREST)
-    return im
-
-if __name__ == '__main__':
+def img2str2():
     im = open_img(IMG)
+    im = im.convert("L")  # 转灰度
     # im.save("./resource/img2gray.jpg")
-    print(u"Width:"+str(im.size[0])+' Height:'+str(im.size[1]))
+    print(u"Width:" + str(im.size[0]) + ' Height:' + str(im.size[1]))
     txt = ""
     for i in range(im.size[1]):
         for j in range(im.size[0]):
             gray = im.getpixel((j, i))
-            txt += ascii_char[int(((count-1)*gray)/256)]
+            txt += ascii_char2[int(((count2 - 1) * gray) / 256)]
         txt += '\n'
 
     print(txt)
-    f = open("output.txt", 'w')
+    f = open("output2.txt", 'w')
     f.write(txt)
     f.close()
+
+def open_img(image_path):
+    im = Image.open(image_path)
+    if im.size[0]>1000 or im.size[1]>1000:
+        im = im.resize((int(im.size[0] * 0.1), int(im.size[1] * 0.05)), Image.NEAREST)
+    return im
+
+
+
+if __name__ == '__main__':
+    img2str1()
+    img2str2()
